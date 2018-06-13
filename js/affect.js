b95058c38a369a6043be058f6e9db8a21230f40f
thazinflower <dreamcatchersucsh@gmail.com>
2018-06-13 17:00:43 +0630

    all affect

--- js/affect.js
+++ js/affect.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	$('button').click(function(){
 		
-		$('#box,#box1,#box2').animate({'width':'300'},1000);
-		$('#box,#box1,#box2').animate({'height':'300'},1000);
-		$('#box,#box1,#box2').animate({'width':'100'},1000);
-		$('#box,#box1,#box2').animate({'height':'100'},1000);
+		$('#box,#box1,#box2,#box3').animate({'width':'300'},1000);
+		$('#box,#box1,#box2,#box3').animate({'height':'300'},1000);
+		$('#box,#box1,#box2,#box3').animate({'width':'100'},1000);
+		$('#box,#box1,#box2,#box3').animate({'height':'100'},1000);
 		
 	});
 });
