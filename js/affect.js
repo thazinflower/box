2ae46dfe2fd5fa7deaa79ed0944c6625e1abd337
thazinflower <dreamcatchersucsh@gmail.com>
2018-06-13 16:43:59 +0630

    all are add effect

--- js/affect.js
+++ js/affect.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	$('button').click(function(){
-		$('#box,#box1').animate({'width':'300'},1000);
-		$('#box,#box1').animate({'height':'300'},1000);
-		$('#box,#box1').animate({'width':'100'},1000);
-		$('#box,#box1').animate({'height':'100'},1000);
+		
+		$('#box,#box1,#box2').animate({'width':'300'},1000);
+		$('#box,#box1,#box2').animate({'height':'300'},1000);
+		$('#box,#box1,#box2').animate({'width':'100'},1000);
+		$('#box,#box1,#box2').animate({'height':'100'},1000);
+		
 	});
 });
