c542deb263326d26785a3a5a714d273a09db09c5
thazinflower <dreamcatchersucsh@gmail.com>
2018-06-13 16:28:10 +0630

    two box affect

--- js/affect.js
+++ js/affect.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$('button').click(function(){
-		$('#box').animate({'width':'300'},1000);
-		$('#box').animate({'height':'300'},1000);
-		$('#box').animate({'width':'100'},1000);
-		$('#box').animate({'height':'100'},1000);
+		$('#box,#box1').animate({'width':'300'},1000);
+		$('#box,#box1').animate({'height':'300'},1000);
+		$('#box,#box1').animate({'width':'100'},1000);
+		$('#box,#box1').animate({'height':'100'},1000);
 	});
 });
